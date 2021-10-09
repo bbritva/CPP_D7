800413d8768efad008397c3cfb7e4a4c8eb3a71a
grvelva <grvelva@student.21-school.ru>
2021-10-09 10:24:41 +0300

    поправил мэк для зависимости от hpp

--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -5,17 +5,17 @@
 
 template <typename T> void swap(T& a, T& b)
 {
-	T c = a;
+	T c(a);
 	a = b;
 	b = c;
 }
 
-template <typename T> T max(T a, T b)
+template <typename T> T max(T const &a, T const &b)
 {
 	return (a > b) ? a : b;
 }
 
-template <typename T> T min(T a, T b)
+template <typename T> T min(T const &a, T const &b)
 {
 	return (a < b) ? a : b;
 }
